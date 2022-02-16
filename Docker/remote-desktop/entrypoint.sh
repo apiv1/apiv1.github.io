6614565ee522902c0e0b24cfe52078484d236a87
xbw <windworst@gmail.com>
2022-02-16 18:12:10 +0800

    update

---
+++ Docker/remote-desktop/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/dumb-init /bin/sh
+
+random() {
+    cat /dev/urandom | base64 | tr -dc '_A-Za-z0-9' | head -c 15
+}
+
+# Create the user account
+export USERNAME=${USERNAME:-user}
+export PASSWORD=${PASSWORD:-$(random)}
+
+echo 'USERNAME='${USERNAME}
+echo 'PASSWORD='${PASSWORD}
+
+groupadd --gid 1020 ${USERNAME}
+useradd --shell /bin/bash --uid 1020 --gid 1020 --password $(openssl passwd ${PASSWORD}) --create-home --home-dir /home/${USERNAME} ${USERNAME}
+usermod -aG sudo ${USERNAME}
+
+# Start xrdp sesman service
+/usr/sbin/xrdp-sesman
+
+# Run xrdp in foreground if no commands specified
+if [ -z "$1" ]; then
+    /usr/sbin/xrdp --nodaemon
+else
+    /usr/sbin/xrdp
+    exec "$@"
+fi

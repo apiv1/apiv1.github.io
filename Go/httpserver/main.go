7a58c22234436eddc5ca85d4751f48a594785f17
xbw <windworst@gmail.com>
2022-10-01 22:59:33 +0800

    update

--- Go/httpserver/main.go
+++ Go/httpserver/main.go
@@ -6,13 +6,22 @@ import (
 	"path/filepath"
 )
 
+type fileServerWrapper struct {
+	handler http.Handler
+}
+
+func (f fileServerWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/octet-stream")
+	f.handler.ServeHTTP(w, r)
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		print("Usage: <dir> [listen-addr]")
 		return
 	}
 	p, _ := filepath.Abs(os.Args[1])
-	http.Handle("/", http.FileServer(http.Dir(p)))
+	http.Handle("/", fileServerWrapper{http.FileServer(http.Dir(p))})
 	addr := ":8088"
 	if len(os.Args) >= 3 {
 		addr = os.Args[2]

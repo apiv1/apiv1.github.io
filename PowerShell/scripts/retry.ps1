072637e0b07295845114f52ba2fdadd87d758a57
xbw <windworst@gmail.com>
2024-03-16 03:13:49 +0800

    update

---
+++ PowerShell/scripts/retry.ps1
@@ -0,0 +1,17 @@
+# https://stackoverflow.com/questions/32348794/how-to-get-status-of-invoke-expression-successful-or-failed
+function retry() {
+  do {
+    try {
+      $global:LASTEXITCODE = 0
+      Invoke-Expression ("$($args -join ' ')")
+      if(!$LASTEXITCODE) {
+        break
+      }
+    }
+    catch {
+      Write-Error $_
+    }
+    Write-Output 'Retrying...'
+    Start-Sleep 1
+  } while (1)
+}

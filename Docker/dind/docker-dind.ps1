5b2cb9f341aa47eb8a0b30328894089b586a32d4
xbw <windworst@gmail.com>
2024-03-20 00:33:27 +0800

    update

--- Docker/dind/docker-dind.ps1
+++ Docker/dind/docker-dind.ps1
@@ -1,10 +1,15 @@
+$DOCKER_BIN = $((Get-Command docker).Path)
+
 function docker-path() {
   $result = $args[0]
   $result = $result -replace '\\', '/'
   $result = $result -replace ':', ''
-  $result = '/' + $result
+  if(!('/' -eq $result[0])) {
+    $result = '/' + $result
+  }
   return $result
 }
+
 function docker-dind() {
   if ($args.Count -lt 1) {
     Write-Output "usage: <DIND_IMAGE> [ARG1] [ARG2] ..."
@@ -17,5 +22,25 @@ function docker-dind() {
   $ttyFlag = if (-not ($env:NO_TTY)) { '-t' } else { $null }
 
   $dockerSock = if (-not ($dockerSock)) { "/var/run/docker.sock" } else { $dockerSock }
-  invoke-expression ("docker run --rm -i $ttyFlag --tmpfs /tmp -v '${dockerSock}:/var/run/docker.sock' -v '${projectDirectory}:${projectDirectory}' -w '${projectDirectory}' -e 'PUID=$uid' -e 'PGID=$gid' -e 'DOCKER_SOCK=$dockerSock' $($args -join ' ')")
+  invoke-expression ("&'$DOCKER_BIN' run --rm -i $ttyFlag --tmpfs /tmp -v '${dockerSock}:/var/run/docker.sock' -v '${projectDirectory}:${projectDirectory}' -w '${projectDirectory}' -e 'PUID=$uid' -e 'PGID=$gid' -e 'DOCKER_SOCK=$dockerSock' $($args -join ' ')")
+}
+
+function docker-compose() {
+  docker-dind apiv1/docker-compose $($args -join ' ')
+}
+
+function docker-buildx() {
+  docker-dind apiv1/docker-buildx $($args -join ' ')
+}
+
+function docker() {
+  if ($args.Count -lt 1) {
+    invoke-expression("&'$DOCKER_BIN'")
+    return
+  }
+  if(Get-Command -ErrorAction SilentlyContinue ("docker-" + $args[0])) {
+    invoke-expression ("docker-$($args -join ' ')")
+  } else {
+    invoke-expression ("&'$DOCKER_BIN' $($args -join ' ')")
+  }
 }

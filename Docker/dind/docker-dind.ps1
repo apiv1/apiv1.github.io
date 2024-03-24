a03f6608147645172d08bb1f3b0ef656ba08d946
xbw <windworst@gmail.com>
2024-03-24 16:17:11 +0800

    update

--- Docker/dind/docker-dind.ps1
+++ Docker/dind/docker-dind.ps1
@@ -1,5 +1,4 @@
 $DOCKER_BIN = $((Get-Command docker).Path)
-
 function docker-path() {
   $result = $args[0]
   $result = $result -replace '\\', '/'
@@ -9,11 +8,13 @@ function docker-path() {
   }
   return $result
 }
-
-function docker-dind() {
+function dind-run() {
   if ($args.Count -lt 1) {
     Write-Output "usage: <DIND_IMAGE> [ARG1] [ARG2] ..."
     return
+  }
+  if(-not ($DIND_QUIET)) {
+    Write-Output '[DIND] Running...'
   }
   $projectDirectory = if (-not ($PROJECT_DIRECTORY)) { $PWD.Path } else { $PROJECT_DIRECTORY }
   $projectDirectory = docker-path $projectDirectory
@@ -26,20 +27,21 @@ function docker-dind() {
   Set-Alias -Name doInvoke -Value $(if (-not ($NO_EXEC)) { 'Invoke-Expression' } else { 'Write-Output' } )
   doInvoke ("&'$DOCKER_BIN' run --rm -i $ttyFlag --tmpfs /tmp -v '${dockerSock}:/var/run/docker.sock' -v '${projectDirectory}:${projectDirectory}' -w '${projectDirectory}' -e 'PUID=$uid' -e 'PGID=$gid' -e 'DOCKER_SOCK=$dockerSock' -e 'PWD=$projectDirectory' $($args -join ' ')")
 }
-
-function docker-dind-login() {
-  docker-dind -v docker-dind-context:/.docker apiv1/dind login $($args -join ' ')
+function dind-docker-compose() {
+  dind-run -v docker-dind-context:/root/.docker apiv1/docker-compose $($args -join ' ')
+}
+function dind-compose() {
+  dind-docker-compose $($args -join ' ')
 }
 
-function docker-compose() {
-  docker-dind -v docker-dind-context:/root/.docker apiv1/docker-compose $($args -join ' ')
+function dind-docker-buildx() {
+  dind-run -v docker-dind-context:/root/.docker apiv1/docker-buildx $($args -join ' ')
+}
+function dind-buildx() {
+  dind-docker-buildx $($args -join ' ')
 }
 
-function docker-buildx() {
-  docker-dind -v docker-dind-context:/root/.docker apiv1/docker-buildx $($args -join ' ')
-}
-
-function docker() {
+function dind-docker() {
   if ($args.Count -lt 1) {
     invoke-expression("&'$DOCKER_BIN'")
     return
@@ -48,5 +50,28 @@ function docker() {
     invoke-expression ("docker-$($args -join ' ')")
   } else {
     invoke-expression ("&'$DOCKER_BIN' $($args -join ' ')")
+  }
+}
+
+function dind-enable() {
+  Set-Alias -Name docker-compose -Value dind-docker-compose -Scope Global
+  Set-Alias -Name docker-buildx -Value dind-docker-buildx -Scope Global
+  Set-Alias -Name docker -Value dind-docker -Scope Global
+}
+function dind-disable() {
+  Remove-Alias -Name docker-compose -Scope Global
+  Remove-Alias -Name docker-buildx -Scope Global
+  Remove-Alias -Name docker -Scope Global
+}
+
+function dind() {
+  if ($args.Count -lt 1) {
+    Write-Output "usage: <docker command in dind>"
+    return
+  }
+  if (Get-Command -ErrorAction SilentlyContinue ("dind-" + $args[0])) {
+    invoke-expression ("dind-$($args -join ' ')")
+  } else {
+    dind-run -v docker-dind-context:/.docker apiv1/dind $($args -join ' ')
   }
 }
